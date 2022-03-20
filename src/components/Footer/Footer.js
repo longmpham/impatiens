1671a31d8476e803b13eba40522452e29c8f5944
Long <longish222@gmail.com>
2022-03-19 22:11:52 -0700

    added header and footer repsonsiveness

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,55 @@
 import React from "react"
 import { BrowserRouter as Router, Link } from "react-router-dom"
 import { GiSpotedFlower } from 'react-icons/gi'
+import { FaArrowCircleUp } from 'react-icons/fa'
 
 import "./Footer.css"
 
 
 const Navbar = () => {
+
+  const [showButton, setShowButton] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  // This function will scroll the window to the top 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    });
+  };
+
+
   return (
     <>
-        <nav className="footer">
+        <footer className="footer">
           <GiSpotedFlower color='yellow' size={70} className="footer-logo" />
           <h1 className="footer-title">Impatiens</h1>
-          <ul>
+          {/* <ul>
             <li className="footer-button"><Link to="/">Home</Link></li>
             <li className="footer-button"><Link to="/about">About</Link></li>
             <li className="footer-button"><Link to="/blog">Blog</Link></li>
             <li className="footer-button"><Link to="/products">Products</Link></li>
             <li className="footer-button"><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
+          </ul> */}
+          {showButton && (
+            // <button onClick={scrollToTop} className="back-to-top"><FaArrowCircleUp size={30}>
+                    // {/* &#8679; */}
+            // </FaArrowCircleUp></button>
+            <FaArrowCircleUp onClick={scrollToTop} className="back-to-top" size={30}>
+            {/* &#8679; */}
+          </FaArrowCircleUp>
+          )}
+        </footer>
     </>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,64 @@
-import React from "react"
-import { BrowserRouter as Router, Link } from "react-router-dom"
-import { GiSpotedFlower, GiHamburgerMenu } from 'react-icons/gi'
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { GiSpotedFlower, GiHamburgerMenu } from "react-icons/gi";
 
-import "./Navbar.css"
-
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = React.useState(false);
+
+  const handleSidebar = () => {
+    setSidebar((prevSidebar) => !prevSidebar);
+    console.log(sidebar);
+  };
+
   return (
     <>
-        <nav className="navbar">
-          <GiSpotedFlower color='yellow' size={70} className="navbar-logo" />
-          <h1 className="navbar-title">Impatiens</h1>
-          <ul>
-            <li className="navbar-button"><Link to="/">Home</Link></li>
-            <li className="navbar-button"><Link to="/about">About</Link></li>
-            <li className="navbar-button"><Link to="/blog">Blog</Link></li>
-            <li className="navbar-button"><Link to="/products">Products</Link></li>
-            <li className="navbar-button"><Link to="/contact">Contact</Link></li>
-            <li className="navbar-button navbar-button-options"><GiHamburgerMenu size={40}/></li>
+      <nav className="navbar">
+        <GiSpotedFlower color="yellow" size={70} className="navbar-logo" />
+        <h1 className="navbar-title">Impatiens</h1>
+        <ul className="navbar-right">
+          <li className="navbar-button">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbar-button">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="navbar-button">
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li className="navbar-button">
+            <Link to="/products">Products</Link>
+          </li>
+          <li className="navbar-button">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <button className="navbar-sidebar-button" onClick={handleSidebar}>
+          <GiHamburgerMenu size={40} />
+        </button>
+        {sidebar && (
+          <ul onClick={handleSidebar} className="navbar-sidebar">
+            <li className="navbar-sidebar-link">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="navbar-sidebar-link">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="navbar-sidebar-link">
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li className="navbar-sidebar-link">
+              <Link to="/products">Products</Link>
+            </li>
+            <li className="navbar-sidebar-link">
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
-        </nav>
+        )}
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
